d231bbeac517ed663f4f157c6e895a9623b3ac9f
Sebastian Legieziński <46867564+seba0456@users.noreply.github.com>
2023-03-07 14:47:31 +0100

    Add files via upload

---
+++ skrypty/order.py
@@ -0,0 +1,12 @@
+import os
+
+folder_path = "Music"
+output_name = "nRadioTrack"
+output_extension = ".wav"
+index = 0
+
+for filename in os.listdir(folder_path):
+    if filename.endswith(output_extension):
+        new_name = f"{output_name}{index:03d}{output_extension}"
+        os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_name))
+        index += 1
